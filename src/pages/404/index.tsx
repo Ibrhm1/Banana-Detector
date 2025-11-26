ba07a9ec366f52a7e1ef6d80fe4f2bd6058f5fc0
Ibrhm1 <iibibrahim2201@gmail.com>
2025-11-26 22:55:38 +0700

    feat: add maintenance and warning illustrations, update LayoutMaintenance and NotFound components

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,14 +1,26 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import LayoutRoot from '~/components/layouts/LayoutRoot';
 import { Button } from '~/components/ui/button';
+import { imageIlustrations } from '~/constants/image-constant';
 
 export default function NotFound() {
   return (
     <LayoutRoot title="Not Found">
-      <main className="h-screen flex flex-col gap-3 items-center justify-center">
-        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-        <Button asChild variant="default">
-          <Link href="/">Go Back</Link>
+      <main className="flex flex-col gap-3 items-center justify-center py-28 bg-linear-to-bl from-yellow-50">
+        <Image
+          alt={imageIlustrations.notFound.alt}
+          src={imageIlustrations.notFound.src}
+          width={300}
+          height={300}
+        />
+
+        <h1 className="text-2xl md:text-4xl font-bold text-center">
+          404 - Page Not Found
+        </h1>
+
+        <Button asChild variant="default" className="font-medium">
+          <Link href="/">Kembali ke beranda</Link>
         </Button>
       </main>
     </LayoutRoot>
